d1139640fe3a085428a96d82b42368f3c416968c
Vineeth <66191662+hteeniv@users.noreply.github.com>
2024-08-14 19:59:21 -0700

    modified VM size of agent pool node

--- AKS/main.tf
+++ AKS/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
 
   default_node_pool {
     name       = "agentpool"
-    vm_size    = "Standard_D2s_v3"
+    vm_size    = "Standard_D2pls_v5"
     node_count = var.node_count
   }
   linux_profile {
